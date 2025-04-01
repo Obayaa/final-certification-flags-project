1385f77ae26d1160f6d600b18e61baa191701b23
Obayaa <doraappiah2004@gmail.com>
2025-04-01 21:21:57 +0000

    Add countries data with flags for World Flags Explorer

---
+++ countries-data.js
@@ -0,0 +1,118 @@
+const countries = [
+    {
+        name: "Afghanistan",
+        flag: "https://upload.wikimedia.org/wikipedia/commons/5/5c/Flag_of_the_Taliban.svg",
+    },
+    {
+        name: "Åland Islands",
+        flag: "https://flagcdn.com/ax.svg",
+    },
+    {
+        name: "Albania",
+        flag: "https://flagcdn.com/al.svg",
+    },
+    {
+        name: "Algeria",
+        flag: "https://flagcdn.com/dz.svg",
+    },
+    {
+        name: "American Samoa",
+        flag: "https://flagcdn.com/as.svg",
+    },
+
+    {
+        name: "Andorra",
+        flag: "https://flagcdn.com/ad.svg",
+    },
+
+    {
+        name: "Angola",
+        flag: "https://flagcdn.com/ao.svg",
+    },
+    {
+        name: "Anguilla",
+        flag: "https://flagcdn.com/ai.svg",
+    },
+    {
+        name: "Antarctica",
+        flag: "https://flagcdn.com/aq.svg",
+    },
+    {
+        name: "Antigua and Barbuda",
+        flag: "https://flagcdn.com/ag.svg",
+    },
+    {
+        name: "Argentina",
+        flag: "https://flagcdn.com/ar.svg",
+    },
+    {
+        name: "Armenia",
+        flag: "https://flagcdn.com/am.svg",
+    },
+    {
+        name: "Aruba",
+        flag: "https://flagcdn.com/aw.svg",
+    },
+    {
+        name: "Australia",
+        flag: "https://flagcdn.com/au.svg",
+    },
+    {
+        name: "Austria",
+        flag: "https://flagcdn.com/at.svg",
+    },
+    {
+        name: "Azerbaijan",
+        flag: "https://flagcdn.com/az.svg",
+    },
+    {
+        name: "Bahamas",
+        flag: "https://flagcdn.com/bs.svg",
+    },
+    {
+        name: "Bahrain",
+        flag: "https://flagcdn.com/bh.svg",
+    },
+    {
+        name: "Bangladesh",
+        flag: "https://flagcdn.com/bd.svg",
+    },
+    {
+        name: "Barbados",
+        flag: "https://flagcdn.com/bb.svg",
+    },
+    {
+        name: "Belarus",
+        flag: "https://flagcdn.com/by.svg",
+    },
+    {
+        name: "Belgium",
+        flag: "https://flagcdn.com/be.svg",
+    },
+    {
+        name: "Belize",
+        flag: "https://flagcdn.com/bz.svg",
+    },
+    {
+        name: "Benin",
+        flag: "https://flagcdn.com/bj.svg",
+    },
+    {
+        name: "Bermuda",
+        flag: "https://flagcdn.com/bm.svg",
+    },
+    {
+        name: "Bhutan",
+        flag: "https://flagcdn.com/bt.svg",
+    },
+    {
+        name: "Bolivia (Plurinational State of)",
+        flag: "https://flagcdn.com/bo.svg",
+    },
+    {
+        name: "Bonaire, Sint Eustatius and Saba",
+        flag: "https://flagcdn.com/bq.svg",
+    },
+];
+
+export { countries };
